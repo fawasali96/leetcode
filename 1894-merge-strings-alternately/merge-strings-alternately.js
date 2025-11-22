c8b80cef321ff2a6f6ff78b1b19aca7d94efc542
Fawas Ali <fawasalikariyeri@gmail.com>
2025-11-22 11:03:22 +0530

    Time: 53 ms (27.26%) | Memory: 54.9 MB (55.18%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let i = 0, j = 0;
+    let result = "";
+    while(i < word1.length && j < word2.length) {
+        result += word1[i++];
+        result += word2[j++]
+    }
+    if(i < word1.length) result += word1.slice(i);
+    if(i < word2.length) result += word2.slice(j);
+    return result;
+};
