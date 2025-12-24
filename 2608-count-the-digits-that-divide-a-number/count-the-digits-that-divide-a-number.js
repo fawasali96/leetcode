6e280bb09121f48e1fd68287fee8b729d50bb734
Fawas Ali <fawasalikariyeri@gmail.com>
2025-12-24 20:30:05 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (94.02%) - LeetSync

---
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countDigits = function(num) {
+    let count = 0;
+    let str = num.toString();
+    for(let ch of str) {
+        if((num % Number(ch)) === 0) count++;
+    }
+    return count;
+};
