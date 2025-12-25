0ae34f8aad650898630489ce4f8c34fa2a47b034
Fawas Ali <fawasalikariyeri@gmail.com>
2025-12-25 23:18:17 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (72.68%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let alt = 0;
+    let maxAlt = 0;
+    for(let g of gain) {
+        alt += g
+        if(alt > maxAlt) {
+            maxAlt = alt;
+        }
+    }
+    return maxAlt;
+};
