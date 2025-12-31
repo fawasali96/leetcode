37dabcf77fb4c290d0c046b7ca01b1c58cef341d
Fawas Ali <fawasalikariyeri@gmail.com>
2025-12-31 19:16:48 +0530

    Time: 23 ms (18.98%) | Memory: 60.4 MB (20.08%) - LeetSync

---
+++ 383-ransom-note/ransom-note.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let count = {};
+    for(let ch of magazine) {
+        count[ch] = ((count[ch] || 0) + 1)
+    }
+    for(let ch of ransomNote) {
+        if(!count[ch]) {
+            return false
+        } 
+        count[ch]--;
+    }
+    return true;
+};
