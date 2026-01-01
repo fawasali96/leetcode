d2da46b02bcd285c4a32d3b006f2acae53ffedc9
Fawas Ali <fawasalikariyeri@gmail.com>
2026-01-01 19:26:09 +0530

    Time: 8 ms (39.83%) | Memory: 56.5 MB (51.81%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function(n) {
+    let row = 1;
+    while(n >= row) {
+        n -= row;
+        row++;
+    }
+    return row - 1;
+};
